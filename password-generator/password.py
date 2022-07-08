30a37796ef8e09738ef75256035dd442953d5f37
Vinicius D. Gubolin <vinicius.demite.gubolin@gmail.com>
2022-07-08 16:03:00 -0300

    refactor: quantidade aleatória de chars refatorada para um função que é chamada somente uma vez

--- password-generator/password.py
+++ password-generator/password.py
@@ -11,24 +11,25 @@ class Password():
   def length(self) -> int:
     return self._length
 
-  @property
-  def chars_qtd(self) -> str:
+  def create_password(self) -> str:
+    chars_qtd = self._chars_qtd()
+    print(chars_qtd)
+    uppercase_chars = random.choices(string.ascii_uppercase, k=chars_qtd)
+    lowercase_chars = random.choices(string.ascii_lowercase, k=chars_qtd)
+    digits = random.choices(string.digits, k=chars_qtd)
+    special_chars = random.choices(
+        self._special_chars, k=chars_qtd
+      )
+
+    password = uppercase_chars + lowercase_chars + digits + special_chars
+    password = random.choices(password, k=self.length)
+
+    return ''.join(password)
+
+  def _chars_qtd(self) -> str:
     new_length = self.length ** random.randint(1, 5)
     new_length -= (self.length + random.uniform(100, 10000))
 
     return round(
-      new_length / self.length * random.randint(2, 10)
+        new_length / self.length * random.randint(2, 10)
     )
-
-  def create_password(self) -> str:
-    uppercase_chars = random.choices(string.ascii_uppercase, k=self.chars_qtd)
-    lowercase_chars = random.choices(string.ascii_lowercase, k=self.chars_qtd)
-    digits = random.choices(string.digits, k=self.chars_qtd)
-    special_chars = random.choices(
-        self._special_chars, k=self.chars_qtd
-      )
-
-    password = uppercase_chars + lowercase_chars + digits + special_chars
-    password = random.choices(random.shuffle(password), k=self.length)
-
-    return ''.join(password)
